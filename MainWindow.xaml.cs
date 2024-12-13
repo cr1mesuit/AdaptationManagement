f7b3542dac3775bfe28f8ef42b99e46c4347d961
Bebrik Bebrov <den-bubnov@ya.ru>
2024-12-13 23:39:29 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+
+namespace AdaptationManagement
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void CreateModule_Click(object sender, RoutedEventArgs e)
+        {
+            CreateModuleWindow createModuleWindow = new CreateModuleWindow();
+            if (createModuleWindow.ShowDialog() == true)
+            {
+                string moduleName = createModuleWindow.ModuleName;
+                string developers = createModuleWindow.Developers;
+                string approvers = createModuleWindow.Approvers;
+                string position = createModuleWindow.Position;
+                DateTime deadline = createModuleWindow.Deadline;
+
+                MessageBox.Show("Модуль успешно создан.", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        public void SendApproval_Click(object sender, RoutedEventArgs e)
+        {}
+
+        public void GenerateProgram_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Программа адаптации отправлена на почту.", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
---
+++ Window1.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+
+namespace AdaptationManagement
+{
+    public partial class CreateModuleWindow : Window
+    {
+        public string ModuleName => ModuleNameTextBox.Text;
+        public string Developers => DevelopersTextBox.Text;
+        public string Approvers => ApproversTextBox.Text;
+        public string Position => PositionTextBox.Text;
+        public DateTime Deadline => DeadlineDatePicker.SelectedDate ?? DateTime.Now;
+
+        public CreateModuleWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            DialogResult = true;
+            Close();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+    }
+}
